660159fd0c587fee532df1ef4144754d89e36aed
BrajeshPandeyOO7 <pbrajesh79@gmail.com>
2023-12-18 01:14:41 +0530

    custome select for size done

--- script.js
+++ script.js
@@ -6,20 +6,61 @@ const getElementByClass = (class_name) => {
     return [...elements]
 }
 
+
 function boxEventHandler(event, id){
     event.stopPropagation()
     let element = document.getElementById(id);
+    const [firstEle ] = element.children;
+    const [ inputEle ] = firstEle.children;
+    
     let active_class = getElementByClass(ACTIVE_MAIN_BOX_CLASS)
     if(active_class.length){
         active_class.forEach(i => {
-            const [,secondSection] = i.children;
-            if(secondSection){
-                secondSection.classList.remove(ACTIVE_SECTION_BOX_CLASS)
+            const [firstSection,secondSection] = i.children;
+            if(firstSection){
+                const [ inputEle ] = firstSection.children;
+                inputEle.checked = false; // reset checked input
             }
-            i.classList.remove(ACTIVE_MAIN_BOX_CLASS)
+            if(secondSection){
+                secondSection.classList.remove(ACTIVE_SECTION_BOX_CLASS) // reset action section class
+            }
+            i.classList.remove(ACTIVE_MAIN_BOX_CLASS);
         })
     }
-    element.classList.add(ACTIVE_MAIN_BOX_CLASS);
     const [,secondSection] = element.children;
-    secondSection.classList.add(ACTIVE_SECTION_BOX_CLASS)
+    secondSection.classList.add(ACTIVE_SECTION_BOX_CLASS);
+    element.classList.add(ACTIVE_MAIN_BOX_CLASS);
+    inputEle.checked = true;
+}
+
+function sizeSelectHandler(event, second_class){
+    event.stopPropagation()
+    const { target } = event;
+    let targetElement = (target?.tagName === 'DIV') ? target.children : target?.parentElement?.children
+    const [selectViewContent,arrow, selectMenu] = targetElement ?? [];
+
+    selectMenu.classList.toggle(second_class)
+
+    const handleSelectMenuItem = (e) => { 
+        e?.stopPropagation();
+        selectViewContent.textContent = e.target.textContent;
+        handleArrowToggle(arrow);
+
+        [...selectMenu.children].forEach(i => { // Once item selected remove the event listner form select menu!
+            i?.removeEventListener('click', handleSelectMenuItem)
+        });
+        selectMenu.classList.toggle(second_class); 
+    }
+    const handleSelectMenuPaper = (element) => { // Open select menu and add eveent listner to every menu
+        [...element.children].forEach(i => {
+            i?.addEventListener('click', handleSelectMenuItem)
+        })
+    }
+
+    const handleArrowToggle = (element) => {
+        console.log(element);
+        element.classList.toggle('toggle-arrow');
+    }
+    handleArrowToggle(arrow);
+    selectMenu && handleSelectMenuPaper(selectMenu)
 }
